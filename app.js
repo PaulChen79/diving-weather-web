1010b9f0b2f6959011a02d38f35e93dcb7a35ab5
Pin Hsu Chen <paul791111@gmail.com>
2022-05-25 22:39:30 +0800

    modify static middleware with path

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 require('./database/config/mongoose')
 const express = require('express')
 const exphbs = require('express-handlebars')
+const path = require('path')
 const routes = require('./routes/index')
 const PORT = process.env.PORT || 3000
 const app = express()
@@ -9,7 +10,7 @@ const app = express()
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
-app.use('/static', express.static(__dirname + '/static'))
+app.use('/static', express.static(path.join(__dirname, 'static')))
 
 app.use(routes)
 
